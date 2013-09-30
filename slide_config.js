c5233d7fb89a3d4d1cc0d91f57825e6f69d97f7c
Sam Dutton <samdutton@gmail.com>
2013-09-30 12:33:10 +0200

    Final rehearsal

--- slide_config.js
+++ slide_config.js
@@ -10,10 +10,10 @@ var SLIDE_CONFIG = {
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     analytics: 'UA-44413410-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
     favIcon: 'images/google_developers_logo_tiny.png',
-    fonts: [
-      'Open Sans:regular,semibold,italic,italicsemibold',
-      'Source Code Pro'
-    ],
+    // fonts: [
+    //   'Open Sans:regular,semibold,italic,italicsemibold',
+    //   'Source Code Pro'
+    // ],
     theme: ['sam'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
